5c275f7a585e65f4f9f8cf1c9096129fb1582ef6
Sergey <serg.hexe@gmail.com>
2026-08-23 18:44:34 +0300

    Use chat bubble for MAX icon

--- src/components/SupportIcon.vue
+++ src/components/SupportIcon.vue
@@ -10,8 +10,7 @@ defineProps({ name: { type: String, required: true } })
     <path fill="currentColor" d="M6.4 14.4h7.3c.5 0 .9.3 1.1.8 2.4 6.8 5.4 9.6 7.2 10.5V15.9c0-1 .8-1.8 1.8-1.8h5.6c.9 0 1.5.7 1.5 1.5v5.5c0 1 .6 1.3 1.2.8 2.5-2.8 4.3-5.6 5.2-7 .2-.4.7-.7 1.2-.7h6.4c1.3 0 2 1.5 1.3 2.5-1.7 2.6-3.9 5.5-6.6 8.5-.7.7-.7 1.8 0 2.5 2.6 2.6 5.1 5.4 7 7.9.7 1 .1 2.3-1.1 2.3h-7c-.5 0-1-.3-1.3-.7-2.3-3.3-4.4-5.7-5.4-5.7-.8 0-.9.7-.9 1.7v3c0 .9-.7 1.7-1.7 1.7h-2.9C15.7 38 8.8 30.8 5.1 16.3c-.2-1 .4-1.9 1.3-1.9Z" />
   </svg>
   <svg v-else-if="name === 'max'" viewBox="0 0 48 48" aria-hidden="true">
-    <path fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" d="M10 35 13 13c.3-2.3 3.3-3.1 4.7-1.2L24 20l6.3-8.2c1.4-1.9 4.4-1.1 4.7 1.2l3 22" />
-    <path fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round" d="M14 30c5.8 5 14.2 5 20 0" />
+    <path fill="none" stroke="currentColor" stroke-width="4.5" stroke-linecap="round" stroke-linejoin="round" d="M23.5 8.5c-8.3 0-15 6.3-15 14.2 0 4.6 2.3 8.7 5.9 11.3l-3.2 6.1 7-3.5c1.7.5 3.5.8 5.3.8 8.3 0 15-6.3 15-14.3S31.8 8.5 23.5 8.5Z" />
   </svg>
   <svg v-else-if="name === 'clock'" viewBox="0 0 48 48" aria-hidden="true">
     <circle cx="24" cy="24" r="17" fill="none" stroke="currentColor" stroke-width="3" />
