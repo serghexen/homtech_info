49e398b357c4842a6253aebab12a4b074fb7d3e2
Sergey <serg.hexe@gmail.com>
2026-08-23 18:52:01 +0300

    Use original JoyCards wordmark

--- src/components/BrandLogo.vue
+++ src/components/BrandLogo.vue
@@ -1,62 +1,40 @@
 <script setup>
-defineProps({ compact: { type: Boolean, default: false } })
+import joyCardsWordmark from '../assets/joycards-wordmark.jpg'
+
+const props = defineProps({ compact: { type: Boolean, default: false } })
+const filterId = props.compact ? 'joy-logo-key-compact' : 'joy-logo-key-full'
 </script>
 
 <template>
-  <span class="joy-logo" :class="{ 'joy-logo--compact': compact }" aria-label="JoyCards">
-    <span class="joy-logo__joy">JOY</span>
-    <span class="joy-logo__cards">CARDS</span>
-    <span class="joy-logo__bolt">✦</span>
+  <span class="joy-logo" :class="{ 'joy-logo--compact': compact }" role="img" aria-label="JoyCards">
+    <svg viewBox="0 0 248 140" aria-hidden="true">
+      <defs>
+        <filter :id="filterId" x="-5%" y="-5%" width="110%" height="110%" color-interpolation-filters="sRGB">
+          <feColorMatrix type="matrix" values="0 0 0 0 1  0 0 0 0 .98  0 0 0 0 .93  .333 .333 .333 0 -.06" />
+        </filter>
+      </defs>
+      <image :href="joyCardsWordmark" width="248" height="140" :filter="`url(#${filterId})`" />
+    </svg>
   </span>
 </template>
 
 <style scoped>
 .joy-logo {
-  position: relative;
-  width: 91px;
-  height: 54px;
-  display: inline-block;
-  color: #fff9ec;
-  font-family: "Trebuchet MS", sans-serif;
-  font-weight: 1000;
-  line-height: .78;
-  letter-spacing: -.1em;
+  width: 118px;
+  height: 66px;
+  display: inline-flex;
+  align-items: center;
+}
+
+.joy-logo svg {
+  width: 100%;
+  height: 100%;
+  display: block;
   filter: drop-shadow(0 4px 9px rgba(0, 0, 0, .24));
-  transform: rotate(-3deg);
-}
-
-.joy-logo__joy,
-.joy-logo__cards {
-  position: absolute;
-  left: 7px;
-  text-shadow: 1px 0 currentColor, -1px 0 currentColor;
-}
-
-.joy-logo__joy {
-  top: 3px;
-  font-size: 25px;
-  transform: rotate(-5deg);
-}
-
-.joy-logo__cards {
-  top: 27px;
-  left: 0;
-  font-size: 24px;
-  transform: rotate(5deg);
-}
-
-.joy-logo__bolt {
-  position: absolute;
-  top: -4px;
-  right: 5px;
-  color: #ff7a3d;
-  font-size: 17px;
 }
 
 .joy-logo--compact {
-  width: 72px;
-  height: 42px;
-  transform: scale(.8) rotate(-3deg);
-  transform-origin: left center;
+  width: 82px;
+  height: 46px;
 }
 </style>
